1f28ca346de4378fac73b4ed2313e09c1ee2559a
SardarBadarSaghir <dataentrybadar@gmail.com>
2020-11-10 00:42:30 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+colors = ["medium blue", "steel blue", "gold", "dark magenta", "dark red", "dark green",
+          "dark slate blue", "sandy brown", "slate gray", "chartreuse", "dodger blue", "saddle brown"
+          ]
